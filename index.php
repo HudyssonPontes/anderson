0f0c1a0c505f9df7692716fe8ae7a30ad50011b3
HudyssonPontes <hudyssonpontes@protonmail.com>
2022-05-16 10:02:40 -0300

    Subindo arquivos iniciais, css, js, index e login

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if(!isset($_SESSION['user'])){
+    die("<script>window.location.href='https://".$_SERVER['HTTP_HOST']."/login.php'</script>");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blingers Analitycs</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+</head>
+<body>
+<div class="limiter" id="login">
+        <div class="container-login100" style="background-image:url(https://image.freepik.com/free-photo/happy-woman-doing-online-shopping-home_329181-4301.jpg)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6"></div>
+                    <div class="col-md-5 col-md-offset-1">
+                        <div class="login_topimg">
+                        </div>
+                        <div class="wrap-login100">
+                            <form class="login100-form validate-form">
+                                <h5 class="text-center text-uppercase"> Login </h5> 
+                                <p class="text-center">Insira suas credenciais</p>
+                                <div class="input_login my-3">
+                                    <input class="form-control" type="email" name="user" placeholder="Usuário">
+                                </div>
+                                <div class="input_login my-3">
+                                    <input class="form-control" type="pass" name="pass" placeholder="Senha">
+                                </div>
+                                <div class="flex-sb-m w-full p-b-30">
+                                    <div class="contact100-form-checkbox">
+                                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                                        <label class="label-checkbox100" for="ckb1">
+                                            Lembrar-me
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <a href="#" class="txt1">
+                                            Esqueceu sua senha?
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="container-login100-form-btn p-t-25"> <button class="login100-form-btn"> Login </button> </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
