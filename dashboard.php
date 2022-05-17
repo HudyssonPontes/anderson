53eae430bb2af2c42113bea8837115d6fa897018
HudyssonPontes <hudyssonpontes@protonmail.com>
2022-05-17 09:59:45 -0300

    criação da página de dashboard inicial

---
+++ dashboard.php
@@ -0,0 +1,48 @@
+<?php require_once("topo.php") ?>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="row">
+        <div class="col-md-2 div_imagem_logotipo">
+            <a href="#" class="text-dark">
+            <div class="row align-items-center">
+                    <div class="col-md-5">
+                        <img class="img-fluid" src="https://img.freepik.com/vetores-gratis/modelo-de-logotipo-da-marca-coyote_23-2149207680.jpg?w=740" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <p class="m-0"><b>blingers</b><br />analytics</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-5">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded active">
+                    <a class="nav-link p-0" href="#">Home</a>
+                </li>
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded">
+                    <a class="nav-link p-0" href="#">Marketplace</a>
+                </li>
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded">
+                    <a class="nav-link p-0" href="#">Vendas</a>
+                </li>
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded">
+                    <a class="nav-link p-0" href="#">Clientes</a>
+                </li>
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded">
+                    <a class="nav-link p-0" href="#">Estoque</a>
+                </li>
+                <li class="nav-item px-2 botao_verde_contorno mx-1 rounded">
+                    <a class="nav-link p-0" href="#">Financeiro</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+</nav>
+</body>
+<?php require_once("rodape.php") ?>
---
+++ rodape.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/slick.js"></script>
+</html>
