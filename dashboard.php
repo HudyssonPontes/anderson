01e4343634fe4c0e6bfad70b7ecb387dd2669817
HudyssonPontes <hudyssonpontes@protonmail.com>
2022-05-18 15:08:48 -0300

    inicio da adição da tabela de Análise por Marketplace

--- dashboard.php
+++ dashboard.php
@@ -187,6 +187,40 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-5">
+        <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Loja</th>
+      <th scope="col">Faturamento</th>
+      <th scope="col">Vendas</th>
+      <th scope="col">Ticket Medio</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
 </div>
 </body>
 <script>
