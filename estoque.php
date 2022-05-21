7771914660931d5efe1f8bbb399803192729117a
HudyssonPontes <hudyssonpontes@protonmail.com>
2022-05-21 12:09:53 -0300

    finalização da página estoque

--- estoque.php
+++ estoque.php
@@ -58,185 +58,285 @@
             </form>
         </div>
     </div>
-    <div class="col-md-5">
+    <div class="col-md-9">
         <div class="row">
             <div class="col">
                 <div class="card_estatistica_menu_dashboard">
-                    <h5>Clientes</h5>
+                    <h5>Quantidade SKU's</h5>
                     <p class="">
-                        10.00 Mil
+                        1.253
                     </p>
                 </div>
             </div>
             <div class="col">
                 <div class="card_estatistica_menu_dashboard">
-                    <h5>Estados</h5>
+                    <h5>SKU's Sem Estoque</h5>
                     <p class="">
-                        26
+                        502
                     </p>
                 </div>
             </div>
             <div class="col">
                 <div class="card_estatistica_menu_dashboard">
-                    <h5>Cidades</h5>
+                    <h5>Valor Estoque (Pc Custo)</h5>
                     <p class="">
-                        2052
+                        R$245,13 Mil
                     </p>
                 </div>
+            </div>
+            <div class="col">
+                <div class="card_estatistica_menu_dashboard">
+                    <h5>Valor Estoque (Pc Venda)</h5>
+                    <p class="">
+                        R$573,85 Mil
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-2 text-center">
+                <label for="periodo_reposicao_do_estoque w-100 text-center">Período reposição do Estoque
+                    <select name="periodo_reposicao_do_estoque w-100" id="periodo_reposicao_do_estoque">
+                        <option value="">1 mes</option>
+                        <option value="">2 meses</option>
+                        <option value="">3 meses</option>
+                        <option value="">4 meses</option>
+                        <option value="">5 meses</option>
+                    </select>
+                </label>
             </div>
         </div>
         <div class="row mt-4">
             <div class="col-md-12 px-0">
-                <div class="div_containers_itens_dashboard ">
-                    <h5>Faturamento (R$) por Região</h5>
-                    <div class="" id="chartBar">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-    <div class="div_containers_itens_dashboard ">
-                <h5>Top 10 Clientes (Por Faturamento)</h5>
+            <div class="div_containers_itens_dashboard ">
                 <div class="tabela_dashboard">
-                    <table class="table tabela_marketplace">
+                    <table class="table tabela_marketplace tabela_estoque">
                         <thead class="thead-light">
                             <tr>
-                                <th scope="col">Cliente</th>
-                                <th scope="col">Faturamento</th>
-                                <th scope="col">Vendas</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">SKU</th>
+                                <th scope="col">Estoque Atual</th>
+                                <th scope="col">Mês 3</th>
+                                <th scope="col">Mês 2</th>
+                                <th scope="col">Mês 1</th>
+                                <th scope="col">Mês Atual</th>
+                                <th scope="col">Média 3 Meses Completos</th>
+                                <th scope="col">Dias em Estoque</th>
+                                <th scope="col">Status Estoque (Dias)</th>
+                                <th scope="col">Sugestão</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Cliente 1</td>
-                                <td>R$1,119,73</td>
-                                <td>11</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 2</td>
-                                <td>R$1,118,80</td>
-                                <td>4</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 3</td>
-                                <td>R$1,121,18</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 4</td>
-                                <td>R$1,049,10</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 5</td>
-                                <td>R$929,70</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 6</td>
-                                <td>R$891,96</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 7</td>
-                                <td>R$1.549,40</td>
-                                <td>2</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 8</td>
-                                <td>R$1.399,90</td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 9</td>
-                                <td>R$1.022,72</td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td>Cliente 10</td>
-                                <td>R$899,73</td>
-                                <td>1</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-    <div class="div_containers_itens_dashboard ">
-                <h5>Top 10 Cidades (Por Faturamento)</h5>
-                <div class="tabela_dashboard">
-                    <table class="table tabela_marketplace">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">Cidade</th>
-                                <th scope="col">Faturamento</th>
-                                <th scope="col">Vendas</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Belo Horizonte - MG</td>
-                                <td>R$10,317,59</td>
-                                <td>41</td>
-                            </tr>
-                            <tr>
-                                <td>São Paulo - SP</td>
-                                <td>R$9.525,02</td>
-                                <td>44</td>
-                            </tr>
-                            <tr>
-                                <td>Rio de Janeiro - RJ</td>
-                                <td>R$3.867,40</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
                                 <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Contagem - MG</td>
-                                <td>R$2.112,69</td>
-                                <td>10</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Brasilia - DF</td>
-                                <td>R$1.867,81</td>
-                                <td>8</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Tocantins - MG</td>
-                                <td>R$1.539,40</td>
-                                <td>2</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Barueri - SP</td>
-                                <td>R$1.255,42</td>
-                                <td>3</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Maringá - PR</td>
-                                <td>R$1.232,42</td>
-                                <td>2</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Florianópolis - SC</td>
-                                <td>R$1.052,31</td>
-                                <td>2</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Nova Alvorada do Sul - MS</td>
-                                <td>R$1.049,10</td>
-                                <td>2</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Blumenau - SC</td>
-                                <td>R$1.034,12</td>
-                                <td>3</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                             <tr>
-                                <td>Osasco - SP</td>
-                                <td>R$1.018,28</td>
-                                <td>3</td>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum dolor sit amet</td>
+                                <td>iudy9832hu</td>
+                                <td>486</td>
+                                <td>341</td>
+                                <td>1341</td>
+                                <td>746</td>
+                                <td>620</td>
+                                <td>810</td>
+                                <td>18</td>
+                                <td>Entre 16 e 30</td>
+                                <td>324</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+            </div>
             </div>
         </div>
     </div>
